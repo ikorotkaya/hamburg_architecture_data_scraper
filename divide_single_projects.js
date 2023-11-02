b693aa41483ab73fc4a64cd6c3228354be8f6083
ikorotkaya <irinkakorotkaya@gmail.com>
2023-11-02 21:50:08 +0100

    Create single projects json file with key-value pairs

---
+++ divide_single_projects.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+
+const inputJSONFile = "parsed_pdf_data.json";
+const outputJSONFile = "single_projects.json";
+
+function extractKeyValuePairs(description) {
+  const keyValuePairs = {};
+  const projects = JSON.parse(fs.readFileSync(inputJSONFile));
+
+  for (const project of projects) {
+    const pdfName = project.pdfName;
+    if (pdfName.includes("2014")) {
+      const finishIndexDescription = description.search(/Führungen:|Termine:/i);
+      if (finishIndexDescription !== -1) {
+        const descriptionText = description.substring(0, finishIndexDescription);
+        keyValuePairs.description = descriptionText;
+      }
+      const startIndexArch = description.search(/Führungen: /i);
+      const finishIndexArch = description.search(/Termine:/i);
+      if (startIndexArch !== -1 && finishIndexArch !== -1) {
+        const architect = description.substring(startIndexArch, finishIndexArch);
+        keyValuePairs.architect = architect.replace("Führungen: ", "");
+      }
+    }
+  }
+
+  // Extract the "Architekten" value
+  const startIndexArch = description.search(/Architekt:|Architekten:|Architekturbüro:|Architekturbüros:|Planungsbüros:/i);
+  const finishIndexArch = description.search(/Führungen:|Termine:/i);
+  if (startIndexArch !== -1 && finishIndexArch !== -1) {
+    const architect = description.substring(startIndexArch, finishIndexArch);
+    keyValuePairs.architect = architect.replace("Architekt: ", "").replace("Architekten: ", "").replace("Architekturbüro: ", "").replace("Architekturbüros: ", "").replace("Planungsbüros: ", "").replace(/\s+$/, "");
+  }
+  
+  // Extract the "Address" value
+  const startIndexAddress = description.search(/Treffpunkt:/i);
+  const finishIndexAddress = description.length;
+  if (startIndexAddress !== -1 && finishIndexAddress !== -1) {
+    const address = description.substring(startIndexAddress, finishIndexAddress);
+    keyValuePairs.address = address.replace("Treffpunkt: ", "").replace(/\s+$/, "");
+  }
+  
+  // Extract the "District" value
+  const startIndexDistrict = 0;
+  const finishIndexDistrict = description.search(/ /i);
+  if (startIndexDistrict !== -1 && finishIndexDistrict !== -1) {
+    const district = description.substring(startIndexDistrict, finishIndexDistrict);
+    keyValuePairs.district = district;
+  }
+  
+  // Extract the description value
+  const finishIndexDescription = description.search(/Architekt:|Architekten:|Architekturbüro:|Architekturbüros:|Planungsbüros:/i);
+  if (finishIndexDescription !== -1) {
+    const descriptionText = description.substring(0, finishIndexDescription);
+    keyValuePairs.description = descriptionText.replace(/\s+$/, "").replace(/^\w+\s*/, "");
+  }
+
+
+  return keyValuePairs;
+}
+
+function divideProjectsInfo() {
+  const projects = JSON.parse(fs.readFileSync(inputJSONFile));
+  const dividedProjects = [];
+
+  for (const project of projects) {
+    const dividedProject = extractKeyValuePairs(project.description.replace(/^\d+\s+/, ""));
+    dividedProject.pdfName = project.pdfName;
+    dividedProject.programmYear = project.pdfName.substring(4, 8);
+    dividedProjects.push(dividedProject);
+  }
+
+  fs.writeFileSync(outputJSONFile, JSON.stringify(dividedProjects));
+  console.log("Single projects: " + dividedProjects.length)
+}
+
+divideProjectsInfo();
