6d20672238b3d5fccd7e8ee2822c7d7181464b3a
ikorotkaya <irinkakorotkaya@gmail.com>
2023-11-21 21:41:00 +0100

    Added try catch to saveImage function, refactor parseWebProjectData function

--- helpers/parseWebProjectData.js
+++ helpers/parseWebProjectData.js
@@ -11,52 +11,48 @@ const parseWebProjectData = async (link) => {
     const html = response.data;
     const $ = cheerio.load(html);
 
-    const imageSrc = $("div.iteminnerimage img").attr("src");
-    
-    
-    if (imageSrc) {
-      const projectData = {};
-      const imageUrl = BASE_URL + imageSrc;
-      projectData.imageUrl = imageUrl;
+    const projectData = {};
 
-      const addressElement = $("div.detail-row:contains('Standort') p");
-      const yearElement = $(
-        "div.detail-row:contains('Jahr der Fertigstellung') p"
-      );
+    const addressElement = $("div.detail-row:contains('Standort') p");
+    const yearElement = $(
+      "div.detail-row:contains('Jahr der Fertigstellung') p"
+    );
 
-      if (
-        !addressElement.text() ||
-        isNaN(Number(yearElement.text())) ||
-        $("div.categories").text().includes("Touren")
-      ) {
-        return null;
-      }
-
-      projectData.title = $("h2").text();
-      projectData.description = $("div.description p").text();
-      projectData.district = $("div.district p").text().trim();
-      projectData.address = addressElement.text().trim();
-      if (!projectData.address.includes("Hamburg")) {
-        projectData.address += ", Hamburg";
-      }
-
-      projectData.architect = $("div.detail-row:contains('Architekturbüro') p")
-        .text()
-        .trim();
-
-      if (projectData.architect.includes("www.")) {
-        const parts = projectData.architect.split(" ");
-        const webAddress = parts.find((part) => part.includes("www."));
-        projectData.architect = projectData.architect
-          .replace(webAddress, "")
-          .replace(/, $/, "");
-      }
-
-      return projectData; 
-    } else {
-      // skip projects without images
+    if (
+      !addressElement.text() ||
+      isNaN(Number(yearElement.text())) ||
+      $("div.categories").text().includes("Touren")
+    ) {
       return null;
     }
+
+    projectData.title = $("h2").text();
+    projectData.description = $("div.description p").text();
+    projectData.district = $("div.district p").text().trim();
+    projectData.address = addressElement.text().trim();
+    if (!projectData.address.includes("Hamburg")) {
+      projectData.address += ", Hamburg";
+    }
+
+    projectData.architect = $("div.detail-row:contains('Architekturbüro') p")
+      .text()
+      .trim();
+
+    if (projectData.architect.includes("www.")) {
+      const parts = projectData.architect.split(" ");
+      const webAddress = parts.find((part) => part.includes("www."));
+      projectData.architect = projectData.architect
+        .replace(webAddress, "")
+        .replace(/, $/, "");
+    }
+
+    const imageSrc = $("div.iteminnerimage img").attr("src");
+    if (imageSrc) {
+      const imageUrl = BASE_URL + imageSrc;
+      projectData.imageUrl = imageUrl;
+    }
+
+    return projectData;
   } catch (error) {
     console.error("Error parsing project data:", error.message);
     return null;
--- helpers/saveImage.js
+++ helpers/saveImage.js
@@ -4,12 +4,16 @@ const download = require("image-downloader");
 const BASE_URL = "https://www.tda-hamburg.de";
 
 const saveImage = async (id, imageUrl) => {
-  const options = {
-    url: imageUrl,
-    dest: path.join(__dirname, `../webp_images/${id}.webp`),
-  };
-  await download.image(options);
-  console.log("Image saved successfully.");
+  try {
+    const options = {
+      url: imageUrl,
+      dest: path.join(__dirname, `../webp_images/${id}.webp`),
+    };
+    await download.image(options);
+    console.log("Image saved successfully.", id);
+  } catch (error) {
+    console.log(error, id);
+  }
 };
 
 module.exports = saveImage;
